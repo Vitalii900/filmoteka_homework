ab7184d6553e84d6e0da8db5f5841e4e0fd01817
Vitalii Melnyk <melnikvitaliy30@gmail.com>
2023-01-11 21:40:30 +0200

    start work

--- src/getPopularMovieFromServer.js
+++ src/getPopularMovieFromServer.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const API_KEY = '197007a676a4d3e84c6bdb6f21b18a1a';
+
 export default async function getPhotoFromServer(value, page) {
   const KEY = '32193446-673e16063f0204736e3ddb7cd';
   try {
@@ -13,3 +15,10 @@ export default async function getPhotoFromServer(value, page) {
     console.error(error);
   }
 }
+
+export default function getPopularMovieFromServer() {
+  fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
+  .then(response => response.json()).then(console.log);
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,62 +1,17 @@
-import getPhotoFromServer from './getPhotoFromServer';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import getPopularMovieFromServer from './getPopularMovieFromServer';
 
-const formRef = document.querySelector('#search-form');
-const galleryRef = document.querySelector('.gallery');
-const loadMoreButtonRef = document.querySelector('.load-more');
+getPopularMovieFromServer();
 
-formRef.addEventListener('submit', searchPhoto);
-loadMoreButtonRef.addEventListener('click', loadMore);
-loadMoreButtonRef.classList.add('visually-hidden');
 
-let pageCounter = 1;
-let inputValue = '';
 
-async function searchPhoto(event) {
-  event.preventDefault();
-  pageCounter = 1;
-  
-  inputValue = event.target.elements.searchQuery.value;
-  const { hits, totalHits } = await getPhotoFromServer(inputValue, pageCounter);
-  
-  galleryRef.innerHTML = '';
 
-  if (hits.length === 0) {
-    Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-    return;
-  } else if (hits.length < 40) {
-    loadMoreButtonRef.classList.add('visually-hidden');
-  } else {
-    loadMoreButtonRef.classList.remove('visually-hidden');
-    Notify.success(`Hooray! We found ${totalHits} images.`);
-  };
 
-  addMurkupOnPage(hits);
-  new SimpleLightbox('.photo-card a');
-}
 
-async function loadMore() {
-  pageCounter += 1;
 
-  const { hits, totalHits } = await getPhotoFromServer(inputValue, pageCounter);
-  let amountOnPage = totalHits - hits.length * pageCounter;
 
-  if (hits.length < 40 || amountOnPage <= 0) {
-    loadMoreButtonRef.classList.add('visually-hidden');
-    Notify.failure(
-      'We are sorry, but you have reached the end of search results'
-    );
-  }
 
-  addMurkupOnPage(hits);
-  // console.log(hits);
-  const lightbox = new SimpleLightbox('.photo-card a');
-  lightbox.refresh();
-}
+
+
 
 function addMurkupOnPage(arrayOfObjects) {
   const murkupFromArray = arrayOfObjects.map(
